84e95fbbc211507dac9448a94524437df69bd36f
TAOYILEE <taoyil@uci.edu>
2019-02-18 22:00:45 -0800

    Fix dataset splitting strategy

--- scripts/train_binary_classifier.py
+++ scripts/train_binary_classifier.py
@@ -1,14 +1,10 @@
-import numpy as np
 import configparser as cp
-from shutil import copyfile
+from core import Preprocessor
 import os
 import argparse
-
-from core.trainer import Trainer
-from core.util.experiments import setup_experiment
-
+from core.util.experiments import ExperimentEnv
 from core.dataset.preprocessing import ECGDataset
-from core.dataset.ecg import BatchGenerator
+from core import SequenceVisualizer, Trainer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,37 +15,17 @@ if __name__ == "__main__":
         print(f"Using configuration file {configuration_file}")
     else:
         raise FileNotFoundError(f"configuration file {configuration_file} dose not exist")
-    np.random.seed(0)
     config = cp.ConfigParser()
     config.read(configuration_file)
-    output_dir, tag = setup_experiment(config["DEFAULT"].get("experiments_dir"))
-    copyfile(configuration_file, os.path.join(output_dir, configuration_file))
-    perct = config["preprocessing"].getfloat("percent_train") / 100
-    percv = config["preprocessing"].getfloat("percent_dev") / 100
-    mitdb_path = config["mitdb"].get("dataset_path")
-    nsrdb_path = config["nsrdb"].get("dataset_path")
+    experiment_env = ExperimentEnv(config)
+    mitdb_path, nsrdb_path = config["mitdb"].get("dataset_path"), config["nsrdb"].get("dataset_path")
+    mitdb = ECGDataset.from_directory(mitdb_path, config["preprocessing"].getint("MIT_DB_TAG"))
+    nsrdb = ECGDataset.from_directory(nsrdb_path, config["preprocessing"].getint("NSR_DB_TAG"))
+    train_generator, dev_generator = Preprocessor(config, experiment_env).preprocess(mitdb, nsrdb)
+    # plot the datasets
+    sv = SequenceVisualizer(config, experiment_env)
+    sv.visualize(train_generator, batch_limit=None, segment_limit=15)
+    sv.visualize(dev_generator, batch_limit=None, segment_limit=15)
 
-    mitdb = ECGDataset.from_directory(mitdb_path, 1)
-    nsrdb = ECGDataset.from_directory(nsrdb_path, 0)
-    mixture_db = mitdb + nsrdb
-    mixture_db.name = "mixture_db"
-    print(mitdb, nsrdb, mixture_db, sep="\n")
-    training_samples = int(perct * len(mixture_db))
-    dev_samples = int(percv * len(mixture_db))
-    test_samples = len(mixture_db) - training_samples - dev_samples
-
-    train_set = mixture_db[:training_samples]  # type: ECGDataset
-    train_set.name = "training_set"
-    dev_set = mixture_db[training_samples:training_samples + dev_samples]  # type: ECGDataset
-    dev_set.name = "development_set"
-    test_set = mixture_db[training_samples + dev_samples:]  # type: ECGDataset
-    test_set.name = "test_set"
-    for data_set in [train_set, dev_set, test_set]:
-        print(data_set)
-        data_set.save(output_dir)
-    train_generator = BatchGenerator(train_set, segment_length=config["preprocessing"].getint("sequence_length"),
-                                     batch_size=config["preprocessing"].getint("batch_size"))
-    dev_generator = BatchGenerator(dev_set, segment_length=config["preprocessing"].getint("sequence_length"),
-                                   batch_size=config["preprocessing"].getint("batch_size"))
-    RNN_Trainer = Trainer(config, output_dir, tag, logger="trainer")
+    RNN_Trainer = Trainer(config, experiment_env, logger="trainer")
     RNN_Trainer.train(train_generator, dev_generator)
