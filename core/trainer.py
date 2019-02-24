d62da99b00732f3d8e4105c601ea0b2575d57bc0
Kenneth Stewart <kennetms@uci.edu>
2019-02-24 13:08:46 -0800

    Merge branch 'input_preprocessing' of C:\Users\kenne\Documents\CS274C\NNProj\cs274c_final_project with conflicts.

---
+++ core/trainer.py
@@ -0,0 +1,67 @@
+from keras.models import Sequential
+from keras.layers import LSTM, BatchNormalization, Dropout, Dense, Bidirectional
+from keras import optimizers
+import os
+from core.action import Action
+import logging
+from core.util.logger import LoggerFactory
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping, TensorBoard, CSVLogger, ModelCheckpoint
+
+
+class Trainer(Action):
+    def __init__(self, config, experiment_env, logger=None):
+        super(Trainer, self).__init__(config, experiment_env)
+        self.logger = LoggerFactory(config).get_logger(logger_name=logger)
+
+    def setup_optimizer(self, model):
+        print(f"*** Adding optimizer to the model ***")
+        adam = optimizers.adam(lr=self.config["RNN-train"].getfloat("initial_lr"))
+        model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['binary_accuracy'])
+
+    def setup_model(self):
+        print(f"*** Setting up deep learning model ***")
+        input_shape = (self.config["preprocessing"].getint("sequence_length"), 2)
+        model = Sequential()
+        model.add((LSTM(self.config["RNN-train"].getint("rnn_output_features")+64+128, input_shape=input_shape, return_sequences=True)))
+        model.add(Dropout(self.config["RNN-train"].getfloat("dropout")))
+        model.add(BatchNormalization())
+        model.add((LSTM(self.config["RNN-train"].getint("rnn_output_features")+64, return_sequences=True)))
+        model.add(Dropout(self.config["RNN-train"].getfloat("dropout")))
+        model.add(BatchNormalization())
+        model.add((LSTM(self.config["RNN-train"].getint("rnn_output_features"))))
+        model.add(Dropout(self.config["RNN-train"].getfloat("dropout")))
+        model.add(BatchNormalization())
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+        self.setup_optimizer(model)
+        return model
+
+    def setup_callbacks(self):
+        print(f"*** Setting up callbacks ***")
+        callbacks = [
+            ModelCheckpoint(os.path.join(self.experiment_env.output_dir, "weights.{epoch:02d}.h5"), monitor='val_loss',
+                            verbose=0,
+                            save_best_only=False, save_weights_only=False, mode='auto', period=1),
+            CSVLogger(os.path.join(self.experiment_env.output_dir, f"training.csv"), separator=',', append=False),
+            ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+                              patience=self.config["RNN-train"].getint("patientce_reduce_lr"),
+                              verbose=self.config["RNN-train"].getint("verbosity"), mode='min',
+                              cooldown=0, min_lr=1e-12),
+            TensorBoard(log_dir=os.path.join(self.config["RNN-train"].get("tensorboard_dir"), self.experiment_env.tag))]
+        if self.config["RNN-train"].getboolean("early_stop"):
+            self.logger.log(logging.INFO, f"Early Stop enabled")
+            callbacks.append(
+                EarlyStopping(monitor='val_loss', min_delta=1e-8, patience=5, verbose=1, mode='min'))
+        else:
+            self.logger.log(logging.INFO, f"Early Stop disabled")
+
+        return callbacks
+
+    def train(self, training_set_generator, dev_set_generator):
+        model = self.setup_model()
+        with open(os.path.join(self.experiment_env.output_dir, "model.json"), "w") as f:
+            f.write(model.to_json())
+        model.fit_generator(generator=training_set_generator, validation_data=dev_set_generator,
+                            epochs=self.config["RNN-train"].getint("epochs"), verbose=1,
+                            callbacks=self.setup_callbacks())
+        model.save(os.path.join(self.experiment_env.output_dir, "final_weights.h5"))
---
+++ scripts/train_binary_classifier.py
@@ -0,0 +1,31 @@
+import configparser as cp
+from core import Preprocessor
+import os
+import argparse
+from core.util.experiments import ExperimentEnv
+from core.dataset.preprocessing import ECGDataset
+from core import SequenceVisualizer, Trainer
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", type=str, help="configuration file")
+    args = parser.parse_args()
+    configuration_file = args.c
+    if os.path.isfile(configuration_file):
+        print(f"Using configuration file {configuration_file}")
+    else:
+        raise FileNotFoundError(f"configuration file {configuration_file} dose not exist")
+    config = cp.ConfigParser()
+    config.read(configuration_file)
+    experiment_env = ExperimentEnv(config)
+    mitdb_path, nsrdb_path = config["mitdb"].get("dataset_path"), config["nsrdb"].get("dataset_path")
+    mitdb = ECGDataset.from_directory(mitdb_path, config["preprocessing"].getint("MIT_DB_TAG"))
+    nsrdb = ECGDataset.from_directory(nsrdb_path, config["preprocessing"].getint("NSR_DB_TAG"))
+    train_generator, dev_generator = Preprocessor(config, experiment_env).preprocess(mitdb, nsrdb)
+    # plot the datasets
+    sv = SequenceVisualizer(config, experiment_env)
+    sv.visualize(train_generator, batch_limit=50, segment_limit=15)
+    sv.visualize(dev_generator, batch_limit=2, segment_limit=15)
+
+    RNN_Trainer = Trainer(config, experiment_env, logger="trainer")
+    RNN_Trainer.train(train_generator, dev_generator)
