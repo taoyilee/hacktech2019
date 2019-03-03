58f8c2558fdc27814f8e2c2785ad3cacc2a6bbff
Michael Tao-Yi Lee <taoyil@uci.edu>
2019-03-02 22:02:26 -0800

    Use NPY to speedup loading

---
+++ scripts/hea2npy.py
@@ -0,0 +1,25 @@
+import wfdb
+import configparser as cp
+import glob
+import os
+import numpy as np
+
+if __name__ == "__main__":
+    config = cp.ConfigParser()
+    config.read("config.ini")
+    mitdb_path, nsrdb_path = config["mitdb"].get("dataset_path"), config["nsrdb"].get("dataset_path")
+    hea = glob.glob(os.path.join(mitdb_path, "*.hea"))
+    for h in hea:
+        hea_record = os.path.splitext(h)[0]
+        hea_base = os.path.split(hea_record)[1]
+        record = wfdb.rdrecord(hea_record)
+        signal = record.p_signal
+        np.save(f"dataset\mitdb\\{hea_base}.npy", signal)
+
+    hea = glob.glob(os.path.join(nsrdb_path, "*.hea"))
+    for h in hea:
+        hea_record = os.path.splitext(h)[0]
+        hea_base = os.path.split(hea_record)[1]
+        record = wfdb.rdrecord(hea_record)
+        signal = record.p_signal
+        np.save(f"dataset\\nsrdb\\{hea_base}.npy", signal)
